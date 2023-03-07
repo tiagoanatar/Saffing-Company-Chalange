fd04b68b766e980baf14404f5be784b5951ca41c
tiagoanatar <tiagoanatar@gmail.com>
2023-03-06 22:38:57 -0300

    Fix tests

--- dpk.js
+++ dpk.js
@@ -8,16 +8,9 @@ function deterministicPartitionKey(event) {
   const MAX_PARTITION_KEY_LENGTH = 256;
   const TRIVIAL_PARTITION_KEY = "0";
 
-  let candidate = event.partitionKey;
-  if (typeof candidate === "number") {
-    candidate = candidate.toString();
-  }
-  if (typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
+  let candidate = event?.partitionKey || crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+  if (typeof candidate !== "string") candidate = JSON.stringify(candidate);
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
 
   return candidate || TRIVIAL_PARTITION_KEY;
 }
